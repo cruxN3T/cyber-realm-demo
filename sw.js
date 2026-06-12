0549498868dd139ca47e5c4b711189616f484c7a
cruxN3T <45500586+cruxN3T@users.noreply.github.com>
2026-06-12 11:06:01 -0700

    Add files via upload
    Cyber Realm Mobile Patch v2.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='cyber-realm-v6';
+const C='cyber-realm-v7';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./game.html','./manifest.json','./icon-192.png','./icon-512.png'])).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{if(e.request.method==='GET'&&n.ok){const cl=n.clone();caches.open(C).then(c=>c.put(e.request,cl));}return n;}).catch(()=>caches.match('./index.html'))));});
